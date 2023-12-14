45d14ffcbc7f4db2c3a5f7b8507b0920bcb6d5a8
mrobirasm <miguel.robiras@konicaminolta.es>
2023-12-14 14:28:07 +0100

    Se crea la primera version

--- soporteKM/Form1.Designer.cs
+++ soporteKM/Form1.Designer.cs
@@ -28,13 +28,103 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.labelSerial = new System.Windows.Forms.Label();
+            this.btnCerrar = new System.Windows.Forms.Button();
+            this.btnSolicitarSoporte = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.labelProgreso = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // labelSerial
+            // 
+            this.labelSerial.AutoSize = true;
+            this.labelSerial.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelSerial.Location = new System.Drawing.Point(110, 33);
+            this.labelSerial.Name = "labelSerial";
+            this.labelSerial.Size = new System.Drawing.Size(70, 26);
+            this.labelSerial.TabIndex = 0;
+            this.labelSerial.Text = "serial";
+            this.labelSerial.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.labelSerial.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // btnCerrar
+            // 
+            this.btnCerrar.Location = new System.Drawing.Point(201, 98);
+            this.btnCerrar.Name = "btnCerrar";
+            this.btnCerrar.Size = new System.Drawing.Size(75, 23);
+            this.btnCerrar.TabIndex = 1;
+            this.btnCerrar.Text = "Cerrar";
+            this.btnCerrar.UseVisualStyleBackColor = true;
+            this.btnCerrar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnSolicitarSoporte
+            // 
+            this.btnSolicitarSoporte.Location = new System.Drawing.Point(62, 98);
+            this.btnSolicitarSoporte.Name = "btnSolicitarSoporte";
+            this.btnSolicitarSoporte.Size = new System.Drawing.Size(118, 23);
+            this.btnSolicitarSoporte.TabIndex = 2;
+            this.btnSolicitarSoporte.Text = "Solicitar soporte";
+            this.btnSolicitarSoporte.UseVisualStyleBackColor = true;
+            this.btnSolicitarSoporte.Click += new System.EventHandler(this.btnSolicitarSoporte_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(62, 148);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(214, 10);
+            this.progressBar1.TabIndex = 3;
+            this.progressBar1.Visible = false;
+            // 
+            // labelProgreso
+            // 
+            this.labelProgreso.AutoSize = true;
+            this.labelProgreso.Location = new System.Drawing.Point(152, 129);
+            this.labelProgreso.Name = "labelProgreso";
+            this.labelProgreso.Size = new System.Drawing.Size(35, 13);
+            this.labelProgreso.TabIndex = 4;
+            this.labelProgreso.Text = "label1";
+            this.labelProgreso.Visible = false;
+            this.labelProgreso.Click += new System.EventHandler(this.labelProgreso_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(99, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(140, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Número de serie";
+            this.label1.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(336, 162);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.labelProgreso);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.btnSolicitarSoporte);
+            this.Controls.Add(this.btnCerrar);
+            this.Controls.Add(this.labelSerial);
+            this.Name = "Form1";
+            this.Text = "SoporteKM";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label labelSerial;
+        private System.Windows.Forms.Button btnCerrar;
+        private System.Windows.Forms.Button btnSolicitarSoporte;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label labelProgreso;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- soporteKM/Form1.cs
+++ soporteKM/Form1.cs
@@ -2,8 +2,13 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Management;
+using System.Net;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,6 +20,124 @@ namespace soporteKM
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            // Obtener el serial del ordenador
+            string serial = ObtenerSerialOrdenador();
+
+            // Mostrar el serial en el Label
+            labelSerial.Text = serial;
+        }
+
+        private string ObtenerSerialOrdenador()
+        {
+            string serial = "";
+
+            try
+            {
+                ManagementObjectSearcher searcher = new ManagementObjectSearcher("SELECT * FROM Win32_BIOS");
+                ManagementObjectCollection collection = searcher.Get();
+
+                foreach (ManagementObject obj in collection)
+                {
+                    serial = obj["SerialNumber"].ToString();
+                    break; // Solo obtenemos el primer resultado
+                }
+
+                // Agrega una declaración de depuración
+                //MessageBox.Show("Serial obtenido: " + serial);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al obtener el serial del ordenador: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            return serial;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Obtener el serial del ordenador
+            string serial = ObtenerSerialOrdenador();
+
+            // Mostrar el serial en el Label
+            labelSerial.Text = serial;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Cierra la ventana Actual
+            this.Close();
+        }
+
+        private void btnSolicitarSoporte_Click(object sender, EventArgs e)
+        {
+            // Establece la visibilidad de los controles
+            labelProgreso.Visible = true;
+            progressBar1.Visible = true;
+
+            // Detección de arquitectura del sistema
+            string url;
+            string programaDescargado;
+
+            if (Environment.Is64BitOperatingSystem)
+            {
+                url = "https://download.teamviewer.com/download/TeamViewerQS_x64.exe";
+                programaDescargado = "TeamViewerQS_x64.exe";
+            }
+            else
+            {
+                url = "https://download.teamviewer.com/download/TeamViewerQS.exe";
+                programaDescargado = "TeamViewerQS.exe";
+            }
+
+            // Descarga del archivo
+            string carpetaTemporal = Path.GetTempPath();
+            string rutaDescarga = Path.Combine(carpetaTemporal, programaDescargado);
+
+            using (WebClient client = new WebClient())
+            {
+                client.DownloadFileCompleted += (s, args) =>
+                {
+                    // Ejecución del programa descargado
+                    EjecutarProgramaDescargado(rutaDescarga);
+
+                    // Eliminar el archivo descargado después de ejecutarlo (opcional)
+                    // File.Delete(rutaDescarga);
+                };
+
+                // Muestra la barra de progreso y la etiqueta
+                progressBar1.Visible = true;
+                labelProgreso.Visible = true;
+
+                client.DownloadProgressChanged += (s, args) =>
+                {
+                    // Actualiza la barra de progreso
+                    progressBar1.Value = args.ProgressPercentage;
+                    labelProgreso.Text = $"Descargando... {args.ProgressPercentage}%";
+                };
+
+                // Descarga el archivo
+                client.DownloadFileAsync(new Uri(url), rutaDescarga);
+            }
+        }
+
+        private void EjecutarProgramaDescargado(string rutaArchivo)
+        {
+            // Ejecuta el programa descargado
+            Process.Start(rutaArchivo);
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void labelProgreso_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
